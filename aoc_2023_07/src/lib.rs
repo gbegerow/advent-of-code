91e7c4d95c2a60ab0b3d28dd273adf9db8068b23
Georg Begerow <gbegerow@googlemail.com>
2023-12-26 10:45:16 +0100

    AOC 2023 07

--- aoc_2023_07/src/lib.rs
+++ aoc_2023_07/src/lib.rs
@@ -1,4 +1,3 @@
-use std::fmt;
 
 // #[allow(dead_code)]
 /* Find the task under https://adventofcode.com/2023/day/07
@@ -11,6 +10,8 @@ use std::fmt;
     If there are 2 cards with the same rank and 2 other cards with the same rank => two_pairs.
     If there are 2 cards with the same rank => one_pair.
     If there are no cards with the same rank => high_card.
+
+    Part b: If there is a joker, the joker can be used rank up the hand.
 */
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
 struct Card {
@@ -19,8 +20,15 @@ struct Card {
 }
 
 impl Card {
-    fn new(label: char) -> Self {
-        let rank = "23456789TJQKA".find(label).expect("invald card label");
+    fn new(label: char, joker: bool) -> Self {
+        let rank = if joker {
+            // joker has rank 0 as indivual card
+            "J23456789TQKA"
+        } else {
+            "23456789TJQKA"
+        }
+        .find(label)
+        .expect("invald card label");
 
         Self { rank, label }
     }
@@ -42,27 +50,29 @@ struct Hand {
     cards: [Card; 5],
     hand_type: HandType,
     bid: usize,
+    joker: bool,
 }
 
 use HandType::*;
 
 impl Hand {
-    fn new(s: &str) -> Self {
+    fn new(s: &str, joker: bool) -> Self {
         let (s_cards, s_bid) = s.trim().split_once(" ").expect("expected cards bid");
         let bid = s_bid.parse().expect("invalid bid");
         let cards: [Card; 5] = s_cards
             .chars()
             .take(5)
-            .map(|c| Card::new(c))
+            .map(|c| Card::new(c, joker))
             .collect::<Vec<_>>()
             .try_into()
             .expect("invalid cards");
 
-        let hand_type = Self::get_handtype(&cards);
+        let hand_type = Self::get_handtype(&cards, joker);
         Self {
             cards,
             hand_type,
             bid,
+            joker,
         }
     }
 
@@ -74,11 +84,16 @@ impl Hand {
         counts
     }
 
-    fn get_handtype(cards: &[Card; 5]) -> HandType {
+    fn get_handtype(cards: &[Card; 5], joker: bool) -> HandType {
+        // could be expressed a lot shorter, might even more readable
         let counts = Self::count_cards(cards);
         let mut hand_type = HighCard;
         let mut pairs = 0;
-        for count in counts.iter() {
+        for count in counts
+            .iter()
+            .enumerate()
+            .filter_map(|(i, &c)| if joker && i==0  { None } else { Some(c) })
+        {
             match count {
                 2 => pairs += 1,
                 3 => hand_type = ThreeOfAKind,
@@ -95,6 +110,27 @@ impl Hand {
         } else if hand_type == HighCard && pairs == 1 {
             hand_type = OnePair;
         }
+
+        if joker {
+            let joker_count = counts[0];
+            // calculate hand type += joker_count except for full house
+            match (joker_count, hand_type) {
+                (1, HighCard) => hand_type = OnePair,
+                (2, HighCard) => hand_type = ThreeOfAKind,
+                (3, HighCard) => hand_type = FourOfAKind,
+                (4, HighCard) => hand_type = FiveOfAKind,
+                (5, HighCard) => hand_type = FiveOfAKind,                
+                (1, OnePair) => hand_type = ThreeOfAKind,
+                (2, OnePair) => hand_type = FourOfAKind,
+                (3, OnePair) => hand_type = FiveOfAKind,
+                (1, TwoPairs) => hand_type = FullHouse,
+                (1, ThreeOfAKind) => hand_type = FourOfAKind,
+                (2, ThreeOfAKind) => hand_type = FiveOfAKind,
+                (1, FourOfAKind) => hand_type = FiveOfAKind,
+                _ => (),
+            }
+        }    
+
         hand_type
     }
 }
@@ -105,19 +141,25 @@ impl PartialOrd for Hand {
     }
 }
 
-
-pub fn aoc_2023_07_a(input: &str) -> usize {
-    let mut hands_down: Vec<_> = input.trim().lines().map(|s| Hand::new(s)).collect();
-    hands_down.sort();
-
-    hands_down
+fn score(input: &str, joker: bool) -> usize {
+    parse(input, joker)
         .iter()
         .enumerate()
         .fold(0, |acc, (i, hand)| acc + hand.bid * (i + 1))
 }
 
-pub fn aoc_2023_07_b(_input: &str) -> usize {
-    0
+fn parse(input: &str, joker: bool) -> Vec<Hand> {
+    let mut hands_down: Vec<_> = input.trim().lines().map(|s| Hand::new(s, joker)).collect();
+    hands_down.sort();
+    hands_down
+}
+
+pub fn aoc_2023_07_a(input: &str) -> usize {
+    score(input, false)
+}
+
+pub fn aoc_2023_07_b(input: &str) -> usize {
+    score(input, true)
 }
 
 #[cfg(test)]
@@ -132,17 +174,17 @@ mod tests {
 
     #[test]
     fn aoc_2023_07_a() {
-        assert_eq!(super::aoc_2023_07_a(INPUT), 0);
+        assert_eq!(super::aoc_2023_07_a(INPUT), 252295678);
     }
 
     #[test]
     fn aoc_2023_07_b_example() {
-        assert_eq!(super::aoc_2023_07_b(TEST_INPUT), 0);
+        assert_eq!(super::aoc_2023_07_b(TEST_INPUT), 5905);
     }
 
     #[test]
     fn aoc_2023_07_b() {
-        assert_eq!(super::aoc_2023_07_b(INPUT), 0);
+        assert_eq!(super::aoc_2023_07_b(INPUT), 250577259);
     }
 
     #[rstest]
@@ -154,9 +196,53 @@ mod tests {
     #[case("QQQJA 483", ThreeOfAKind)]
     #[case("JJJJJ 001", FiveOfAKind)]
     #[case("QQQQ2 483", FourOfAKind)]
-    fn handtype_should_be_correct(#[case] cards: &str, #[case] expected: crate::HandType) {
-        let sut = super::Hand::new(cards);
+    fn handtype_should_be_correct_a(#[case] cards: &str, #[case] expected: crate::HandType) {
+        let sut = super::Hand::new(cards, false);
         assert_eq!(sut.hand_type, expected);
+    }
+
+    #[rstest]
+    #[case("23456 213", HighCard)]
+    #[case("J3456 213", OnePair)]
+    #[case("2345J 213", OnePair)]    
+    #[case("32T3K 765", OnePair)]
+    #[case("T55J5 684", FourOfAKind)]
+    #[case("KK677 28", TwoPairs)]
+    #[case("KTJJT 220", FourOfAKind)]
+    #[case("QQQJA 483", FourOfAKind)]
+    #[case("JJJJJ 001", FiveOfAKind)]
+    #[case("QQQQJ 001", FiveOfAKind)]
+    #[case("JQQQQ 001", FiveOfAKind)]
+    #[case("QQQQ2 483", FourOfAKind)]
+    fn handtype_should_be_correct_b(#[case] cards: &str, #[case] expected: crate::HandType) {
+        let sut = super::Hand::new(cards, true);
+        assert_eq!(sut.hand_type, expected);
+    }
+
+    #[test]
+    fn hands_should_be_sorted() {
+        let sut = super::parse(TEST_INPUT, true);
+        assert_eq!(sut, 
+            vec![
+                super::Hand::new("32T3K 765", true),
+                super::Hand::new("KK677 28", true),
+                super::Hand::new("T55J5 684", true),
+                super::Hand::new("QQQJA 483", true),
+                super::Hand::new("KTJJT 220", true),
+            ]
+        );
+    }
+
+    #[test]
+    fn hands_leading_joker_should_be_sorted() {
+        let sut = super::parse("AAAAA 001\nJJJJJ 002\n22222 003", true);
+        assert_eq!(sut, 
+            vec![
+                super::Hand::new("JJJJJ 002", true),
+                super::Hand::new("22222 003", true),
+                super::Hand::new("AAAAA 001", true),
+            ]
+        );
     }
 
     const INPUT: &str = include_str!("input.txt");
