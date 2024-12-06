55095b6c20cf81d8271baa43babeb47d0e2eb0e3
Georg Begerow <gbegerow@googlemail.com>
2024-12-06 12:32:45 +0100

    2024 05

--- aoc_2024_05/src/lib.rs
+++ aoc_2024_05/src/lib.rs
@@ -2,12 +2,28 @@
 /* Find the task under https://adventofcode.com/2024/day/05
 Solution idea:
 rules build a directed graph wich we could toplogical sort for the right order
-unccesary complex
+unccesary complex? after solving b, YES!!!
 
 find all rule violation for print
+for sorting in b we need topological order
+
+input rules seems to have cycles 😱
+Lots of cycles
+
+ make dot from input:
+    Insert by "digraph {", remove prints,  append "}"
+    node labels off with  node[label=""];
+    Replace | with ->, insert ; at end of lines
+
+    dot -Tsvg -O -Kcirco rules.dot
+
+
 */
 
-use std::collections::{btree_map::Keys, HashMap, HashSet, VecDeque};
+use std::{
+    cmp::Ordering,
+    collections::{HashSet, VecDeque},
+};
 
 fn parse(input: &str) -> (Vec<(u32, u32)>, Vec<Vec<u32>>) {
     let Some((rules_def, pages_def)) = input.split_once("\n\n") else {
@@ -35,24 +51,27 @@ fn parse(input: &str) -> (Vec<(u32, u32)>, Vec<Vec<u32>>) {
     (rules, prints)
 }
 
+#[allow(dead_code)]
 fn get_without_incoming(g: &Vec<(u32, u32)>) -> Vec<u32> {
-    let on_right = g.iter().map(|(_, r)| r).collect::<HashSet>();
+    let on_right = g.iter().map(|(_, r)| r).collect::<HashSet<_>>();
     g.iter()
         .filter(|(l, _)| !on_right.contains(l))
-        .map(|(l, _)| l)
+        .map(|(l, _)| *l)
         .collect()
 }
 
+#[allow(dead_code)]
 fn topo_sort(rules: &Vec<(u32, u32)>) -> Vec<u32> {
     // Kahn' algorithm https://en.wikipedia.org/wiki/Topological_sorting
 
     let mut g = rules.clone();
     let mut sorted = Vec::with_capacity(rules.len() * 2);
-    let on_right = g.iter().map(|(_, r)| r).collect::<HashSet>();
     let mut no_incoming = VecDeque::from(get_without_incoming(&g));
 
-    while let n = no_incoming.pop_front() {
-        sorted.push(n);
+    while let Some(n) = no_incoming.pop_front() {
+        if !sorted.contains(&n) {
+            sorted.push(n);
+        }
 
         // remove outgoing edges of n from graph
         g.retain(|&(l, _)| l != n);
@@ -60,8 +79,10 @@ fn topo_sort(rules: &Vec<(u32, u32)>) -> Vec<u32> {
         no_incoming.extend(get_without_incoming(&g));
     }
 
-    assert!(g.len() == 0, "graph contains cycle!");
-    dbg!(sorted)
+    // the real input does contain a cycle! Do we need to care? Unfoutunately ayes
+    //assert!(g.len() == 0, "graph contains cycle!");
+    println!("sorted: {}  {:?} \nrest: {:?}", sorted.len(), sorted, g);
+    sorted
 }
 
 #[allow(dead_code)]
@@ -128,58 +149,61 @@ pub fn aoc_2024_05_a(input: &str) -> u32 {
     res
 }
 
-#[tracing::instrument]
-pub fn aoc_2024_05_b(input: &str) -> u32 {
-    let (rules, prints) = parse(input);
-    let sorted = topo_sort(&rules);
-    let sorted_index = sorted
-        .iter()
-        .enumerate()
-        .map(|(i, n)| (n, i))
-        .collect::<HashMap<_, _>>();
-
-    let mut res = 0;
-    for mut print in prints {
-        if !print.is_sorted_by_key(|page| sorted_index.get(page).or(Some(page as usize))) {
-            print.sort_by_key(|page| sorted_index.get(page).or(Some(page as usize)));
-            // find the middle pagenumber
-            res += print[print.len() / 2];
-        }
-    }
-    res
-}
-
+// #[tracing::instrument]
 // pub fn aoc_2024_05_b(input: &str) -> u32 {
 //     let (rules, prints) = parse(input);
+//     let sorted = topo_sort(&rules);
+//     let sorted_index = sorted
+//         .iter()
+//         .enumerate()
+//         .map(|(i, n)| (n, i as u32))
+//         .collect::<HashMap<_, _>>();
 
 //     let mut res = 0;
 //     for mut print in prints {
-//         // if violations are in topological order just swap left and right in print
-//         // brute force: just swap the first and recalc violations
-//         // todo: Remove INFINTY loop!!!! tomorrow
-//         let mut swaps = 0;
-//         while let Some((l, r)) = get_first_violation(&print, &rules) {
-//             let i = print
-//                 .iter()
-//                 .position(|n| *n == r)
-//                 .expect("violation right does not occur in print");
-//             // it is a violation, l must occur after r
-//             let j = print[(i + 1)..]
-//                 .iter()
-//                 .position(|n| *n == l)
-//                 .expect("violation left does not occur in print");
+//         // rules definition contains lots of circular definitions
+//         // filter the rules by use pages does not solve the circles.
+//         // what now???
 
-//             swaps += 1;
-//             print.swap(i, j);
-//         }
-
-//         if swaps > 0 {
-//             // was faulted but now it is corrected
+//         if !print.is_sorted_by_key(|page| sorted_index.get(page).unwrap_or(page)) {
+//             // if page is constraint use index else use page number itself as order is not important
+//             print.sort_by_key(|page| sorted_index.get(page).unwrap_or(page).clone());
+//             // find the middle pagenumber
 //             res += print[print.len() / 2];
 //         }
 //     }
 //     res
 // }
+
+#[tracing::instrument]
+pub fn aoc_2024_05_b(input: &str) -> u32 {
+    let (rules, prints) = parse(input);
+    let rule_set = rules.iter().cloned().collect::<HashSet<_>>();
+
+    let mut res = 0;
+    for print in prints {
+        // is ok, already handled in part 1?
+        if check_all(&print, &rules) {
+            continue;
+        }
+
+        let mut print = print;
+        print.sort_by(|a, b| {
+            // is there a rule that says a must be before b? The rules are just sorting rules 🤦
+            // optimizing candidate, make contains test faster, eg. hashset.
+            // just about double the performance with standard HashSet (aka HashBrown) vs linear search (1.349 ms vs 3.689 ms)
+            if rule_set.contains(&(*a, *b)) {
+                Ordering::Greater
+            } else {
+                Ordering::Less
+            }
+        });
+
+        // was faulted but now it is corrected
+        res += print[print.len() / 2];
+    }
+    res
+}
 
 pub const INPUT: &str = include_str!("input.txt");
 
@@ -206,7 +230,7 @@ mod tests {
 
     #[test]
     fn aoc_2024_05_b() {
-        assert_eq!(super::aoc_2024_05_b(super::INPUT), 0);
+        assert_eq!(super::aoc_2024_05_b(super::INPUT), 5169);
     }
 
     const TEST_INPUT: &str = "47|53
